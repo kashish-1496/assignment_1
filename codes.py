aa3e0ff4ed23860a62765da5113cefad6110f1f5
kashish agrawal <135851385+kashish-1496@users.noreply.github.com>
2023-09-01 23:00:26 +0530

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+
+#calculating floor division
+print("Floor division is:",a//b)
+
+#calculating power
+print("A raise to power b is:",a**b)
+
+#checking equality
+if(a==b):
+    print("True")
+else:
+    print("False")
+
